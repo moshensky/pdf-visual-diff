d1f70f78fe9453cb50e6f468a171a85b2029b59b
Nikita Moshensky <]=[poikj-0o98u7hy;6lkjhgfdx>
2020-09-11 17:05:19 +0300

    publish 0.2.1

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,48 +2,48 @@
 
 set -e
 
-# usage()
-# {
-#     echo "usage: publish [[[--set-version X.X.X ] [-i]] | [-h]]"
-# }
+usage()
+{
+    echo "usage: publish [[[--set-version X.X.X ] [-i]] | [-h]]"
+}
 
-# new_version=
+new_version=
 
-# while [ "$1" != "" ]; do
-#     case $1 in
-#         --set-version )           shift
-#                                 new_version=$1
-#                                 ;;
-#         -h | --help )           usage
-#                                 exit
-#                                 ;;
-#         * )                     usage
-#                                 exit 1
-#     esac
-#     shift
-# done
+while [ "$1" != "" ]; do
+    case $1 in
+        --set-version )           shift
+                                new_version=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
 
-# if [ -z "$new_version" ]; then
-#     usage
-#     exit 1
-# fi
+if [ -z "$new_version" ]; then
+    usage
+    exit 1
+fi
 
-if [ "$(git describe --contains --all HEAD)" != "master" ]; then
+if [ "$(git branch --show-current)" != "master" ]; then
   echo "Git branch is NOT master!"
   exit 1
 fi
 
-if [ ! -z "$(git status --porcelain)" ]; then
-  echo "Git working tree is NOT clean!"
-  exit 1
-fi
+# if [ ! -z "$(git status --porcelain)" ]; then
+#   echo "Git working tree is NOT clean!"
+#   exit 1
+# fi
 
-# # Replace version in package.json and package-lock.json files
-# for package_path in package.json package-lock.json
-# do
-#     awk '{if (match($0, /^  "version"/) > 0) print "  \"version\": \"'${new_version}'\"," ;else print}' $package_path > tmp
-#     mv tmp $package_path
-# done
+# Replace version in package.json and package-lock.json files
+for package_path in package.json package-lock.json
+do
+    awk '{if (match($0, /^  "version"/) > 0) print "  \"version\": \"'${new_version}'\"," ;else print}' $package_path > tmp
+    mv tmp $package_path
+done
 
 rm -rf node_modules
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -51,11 +51,12 @@ nvm use
 npm ci
 npm run build
 npm pack
-npm publish
+# npm publish
 
-# git tag $new_version
-git tag "$(jq -r .version package.json)"
-git push origin --tags
+git add . -A
+git commit -m 'publish '${new_version}
+git tag $new_version
+git push --tags
 
 echo "Success"
 exit 0
