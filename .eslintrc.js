c141449dae92597eb06effc0963e063c2a804e8e
Nikita Moshensky <nikita@moshensky.com>
2024-09-01 20:02:11 +0300

    add eslint-plugin-tsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-tsdoc'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    'tsdoc/syntax': 'warn',
+  },
 }
